b9bd223773a9aa19d53500cdf256a9d0748d30ff
Timwi <github@timwi.de>
2019-10-19 04:17:51 +0200

    Add rule-seed support and TP support.

--- Assets/NandMs.cs
+++ Assets/NandMs.cs
@@ -1,112 +1,146 @@
 using System;
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using KModkit;
+using System.Text.RegularExpressions;
 
 public class NandMs : MonoBehaviour
- {
-	 	public KMAudio Audio;
-		public KMBombInfo bomb;
+{
+    public KMAudio Audio;
+    public KMBombInfo bomb;
+    public KMRuleSeedable ruleSeedable;
 
-		static int moduleIdCounter = 1;
-		int moduleId;
-		bool moduleSolved;
+    static int moduleIdCounter = 1;
+    int moduleId;
+    bool moduleSolved;
     bool recalcing;
-    public String[] allWords;
+    string[] allWords;
     public Color[] textColors;
     public KMSelectable[] buttons;
     public TextMesh[] buttonWords;
 
-    public String[][] sets;
-    public string[] row1, row2, row3, row4, row5, column1, column2, column3, column4, column5;
-    List <int> decidedButtons = new List <int>();
-    List <int> decidedWords = new List <int>();
-    public List <int> numbers;
+    string[][] sets;
+    List<int> decidedButtons = new List<int>();
+    List<int> decidedWords = new List<int>();
 
     int setIndex;
     int otherwordindex;
-    string [] otherWords;
+    string[] otherWords;
 
-		void Awake()
-		{
+    void Awake()
+    {
         moduleId = moduleIdCounter++;
         foreach (KMSelectable button in buttons)
         {
-          button.OnInteract += delegate () { buttonPress(button); return false; };
+            button.OnInteract += delegate () { buttonPress(button); return false; };
         }
-		}
 
-		void Start ()
-		{
-      sets = new[] { row1, row2, row3, row4, row5, column1, column2, column3, column4, column5 };
-      setIndex = UnityEngine.Random.Range(0,10);
-      otherwordindex = UnityEngine.Random.Range(0,20);
-      otherWords = allWords.Except(sets[setIndex]).ToArray();
-      pickWords();
-		}
+        var rnd = ruleSeedable.GetRNG();
+        var allStrings = new List<string>();
+        for (var strNum = 0; strNum < 32; strNum++)
+        {
+            var strNum2 = strNum;
+            var str = "";
+            for (var b = 0; b < 5; b++)
+            {
+                str += ((strNum2 & 1) == 1) ? "M" : "N";
+                strNum2 >>= 1;
+            }
+            allStrings.Add(str);
+        }
+        rnd.ShuffleFisherYates(allStrings);
+
+        sets = new string[10][];
+
+        for (int r = 0; r < 5; r++)
+            sets[r] = Enumerable.Range(0, 5).Select(c => allStrings[c + 5 * r]).ToArray();
+        for (int c = 0; c < 5; c++)
+            sets[5 + c] = Enumerable.Range(0, 5).Select(r => allStrings[c + 5 * r]).ToArray();
+
+        allWords = allStrings.Take(25).ToArray();
+    }
+
+    void Start()
+    {
+        setIndex = UnityEngine.Random.Range(0, 10);
+        otherwordindex = UnityEngine.Random.Range(0, 20);
+        otherWords = allWords.Except(sets[setIndex]).ToArray();
+        pickWords();
+    }
 
     void buttonPress(KMSelectable button)
     {
-      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, button.transform);
-      button.AddInteractionPunch(.5f);
-      if(moduleSolved || recalcing)
-      {
-        return;
-      }
-      if(otherWords[otherwordindex] != button.GetComponentInChildren<TextMesh>().text)
-      {
-        GetComponent<KMBombModule>().HandleStrike();
-        Debug.LogFormat("[N&Ms #{0}] Strike! Resetting...", moduleId);
-        Start();
-      }
-      else
-      {
-        GetComponent<KMBombModule>().HandlePass();
-        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
-        Debug.LogFormat("[N&Ms #{0}] Module solved.", moduleId);
-        moduleSolved = true;
-        StartCoroutine(showWords());
-      }
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, button.transform);
+        button.AddInteractionPunch(.5f);
+        if (moduleSolved || recalcing)
+        {
+            return;
+        }
+        if (otherWords[otherwordindex] != button.GetComponentInChildren<TextMesh>().text)
+        {
+            GetComponent<KMBombModule>().HandleStrike();
+            Debug.LogFormat("[N&Ms #{0}] You pressed {1}. Strike! Resetting...", moduleId, button.GetComponentInChildren<TextMesh>().text);
+            Start();
+        }
+        else
+        {
+            GetComponent<KMBombModule>().HandlePass();
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+            Debug.LogFormat("[N&Ms #{0}] Module solved.", moduleId);
+            moduleSolved = true;
+            StartCoroutine(showWords());
+        }
     }
 
     void pickWords()
     {
-      decidedButtons = Enumerable.Range(0,5).ToList().Shuffle().Take(4).ToList();
-      decidedWords = Enumerable.Range(0,5).ToList().Shuffle().Take(4).ToList();
-      Debug.LogFormat("[N&Ms #{0}] The correct word to press is {1}.", moduleId, otherWords[otherwordindex]);
-      StartCoroutine(showWords());
+        decidedButtons = Enumerable.Range(0, 5).ToList().Shuffle().Take(4).ToList();
+        decidedWords = Enumerable.Range(0, 5).ToList().Shuffle().Take(4).ToList();
+        Debug.LogFormat("[N&Ms #{0}] The correct word to press is {1}.", moduleId, otherWords[otherwordindex]);
+        StartCoroutine(showWords());
     }
 
     IEnumerator showWords()
     {
-      recalcing = true;
-      for(int i = 0; i <= 4; i++)
-      {
-        buttonWords[i].text = "";
-        yield return new WaitForSeconds(.3f);
-      }
-      if(!moduleSolved)
-      {
-        yield return new WaitForSeconds(.2f);
-        for(int i = 0; i <= 4; i++)
+        recalcing = true;
+        for (int i = 0; i <= 4; i++)
         {
-          int colorIndex = UnityEngine.Random.Range(0,6);
-          buttonWords[i].color = textColors[colorIndex];
-          int ix = decidedButtons.IndexOf(i);
-          if(ix == -1)
-          {
-            buttonWords[i].text = otherWords[otherwordindex];
-          }
-          else
-          {
-            buttonWords[i].text = sets[setIndex][decidedWords[ix]];
-          }
-          yield return new WaitForSeconds(.3f);
+            buttonWords[i].text = "";
+            yield return new WaitForSeconds(.3f);
         }
-      }
-      recalcing = false;
+        if (!moduleSolved)
+        {
+            yield return new WaitForSeconds(.2f);
+            for (int i = 0; i <= 4; i++)
+            {
+                int colorIndex = UnityEngine.Random.Range(0, 6);
+                buttonWords[i].color = textColors[colorIndex];
+                int ix = decidedButtons.IndexOf(i);
+                if (ix == -1)
+                {
+                    buttonWords[i].text = otherWords[otherwordindex];
+                }
+                else
+                {
+                    buttonWords[i].text = sets[setIndex][decidedWords[ix]];
+                }
+                yield return new WaitForSeconds(.3f);
+            }
+        }
+        recalcing = false;
     }
 
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press 1/2/3/4/5 [presses the button in that position from top to bottom]";
+#pragma warning restore 414
+
+    KMSelectable[] ProcessTwitchCommand(string command)
+    {
+        Match m;
+        if ((m = Regex.Match(command, @"^\s*(?:press\s+)?([1-5])$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success)
+            return new[] { buttons[int.Parse(m.Groups[1].Value) - 1] };
+        return null;
+    }
 }
