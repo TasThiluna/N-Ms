e11ce3b14d5e081588fe9a473ef0f0ca79d7040c
TasThing <lufarionite@gmail.com>
2020-05-08 20:56:53 -0400

    Progress

--- Assets/MandNs.cs
+++ Assets/MandNs.cs
@@ -16,6 +16,16 @@ public class MandNs : MonoBehaviour
     public KMSelectable[] buttons;
     public TextMesh[] buttonWords;
 
+    private int[] buttonColors = new int[5];
+    private List<Char> ser = new List<Char>();
+    private string zeroLetter;
+    private string oneLetter;
+    private int snBinary;
+
+    private static readonly string base36String = "0123456789ABCDEFGHJIKLMNOPQRSTUVWXYZ";
+    private static Char[] base36 = new Char[36];
+    private static readonly string[] colorNames = new string[] { "red", "green", "orange", "blue", "yellow", "brown" };
+
     private static int moduleIdCounter = 1;
     private int moduleId;
     private bool moduleSolved;
@@ -25,11 +35,24 @@ public class MandNs : MonoBehaviour
         moduleId = moduleIdCounter++;
         foreach (KMSelectable button in buttons)
             button.OnInteract += delegate () { ButtonPress(button); return false; };
+        base36 = base36String.ToCharArray();
     }
 
     void Start()
     {
-
+        zeroLetter = bomb.GetBatteryCount() % 2 == 0 ? "M" : "N";
+        oneLetter = bomb.GetBatteryCount() % 2 == 0 ? "N" : "M";
+        regenerate:
+        for (int i = 0; i < 5; i++)
+            buttonColors[i] = rnd.Range(0, 6);
+        ser = bomb.GetSerialNumber().ToCharArray().ToList();
+        ser.Remove(ser[buttonColors[1]]);
+        var binaryAdditions = new int[] { 16, 8, 4, 2, 1 };
+        for (int i = 5; i < 5; i++)
+            if (Array.IndexOf(base36, ser[i]) % 2 == 1)
+                snBinary += binaryAdditions[i];
+        Debug.Log((buttonColors[1] + 1).ToString());
+        Debug.Log(snBinary.ToString());
     }
 
     void ButtonPress(KMSelectable button)
