ade41684df6e12ee5e9847677253f247a2446acf
Timwi <github@timwi.de>
2020-06-27 21:24:59 +0200

    M&Ms rule seed (both JS and C#)

--- Assets/MandMs.cs
+++ Assets/MandMs.cs
@@ -11,6 +11,7 @@ public class MandMs : MonoBehaviour
     public new KMAudio audio;
     public KMBombInfo bomb;
     public KMBombModule module;
+    public KMRuleSeedable RuleSeedable;
 
     public Color[] textColors;
     public KMSelectable[] buttons;
@@ -21,8 +22,8 @@ public class MandMs : MonoBehaviour
     private int stage;
     private int gridIndex;
     private int rotationIndex;
-    private Char whiteLetter;
-    private Char blackLetter;
+    private char whiteLetter;
+    private char blackLetter;
 
     private static readonly string[] ordinals = new string[9] { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth" };
     private static readonly string[] rotationNames = new string[4] { "not rotated", "rotated 90° clockwise", "rotated 180°", "rotated 90° counterclockwise" };
@@ -34,13 +35,80 @@ public class MandMs : MonoBehaviour
     private int moduleId;
     private bool moduleSolved;
 
+    // Rule seed
+    private bool[][] grids;
+
     void Awake()
     {
         moduleId = moduleIdCounter++;
         foreach (KMSelectable button in buttons)
             button.OnInteract += delegate () { ButtonPress(button); return false; };
         module.OnActivate += delegate () { StartCoroutine(ShowWords()); };
+
+        var rnd = RuleSeedable.GetRNG();
+        Debug.LogFormat("[M&Ms #{0}] Using rule seed: {1}.", moduleId, rnd.Seed);
+        grids = new bool[9][];
+        var gridsAlready = new List<bool[]>();
+
+        for (var gridIx = 0; gridIx < 9; gridIx++)
+        {
+            var grid = findGrid(new bool[25], 0, gridsAlready, rnd);
+            if (grid == null)
+            {
+                Debug.LogFormat("<M&Ms #{0}> Fatal error: no grid!", moduleId);
+                throw new InvalidOperationException();
+            }
+            grids[gridIx] = grid;
+            Debug.LogFormat("<M&Ms #{0}> Grid #{1}: {2}", moduleId, gridIx, grid.Select(b => b ? "█" : "░").Join(""));
+
+            for (var rot = 0; rot < 4; rot++)
+            {
+                gridsAlready.Add(grid);
+                grid = rotate(grid);
+            }
+        }
     }
+
+    private static bool[] rotate(bool[] grid, int numberOfTimes = 1)
+    {
+        for (var n = 0; n < numberOfTimes; n++)
+            grid = grid.Select((_, i) => grid[(i % 5) * 5 + 4 - (i / 5)]).ToArray();
+        return grid;
+    }
+
+    bool[] findGrid(bool[] grid, int ix, List<bool[]> gridsAlready, MonoRandom rnd)
+    {
+        if (ix % 5 == 0)
+            for (var prevRow = 0; prevRow * 5 < ix - 5; prevRow++)
+                if (Enumerable.Range(0, 5).All(x => grid[prevRow * 5 + x] == grid[ix - 5 + x]))
+                    return null;
+        if (ix == 25)
+        {
+            for (var col = 0; col < 5; col++)
+                for (var col2 = 0; col2 < col; col2++)
+                    if (Enumerable.Range(0, 5).All(y => grid[y * 5 + col] == grid[y * 5 + col2]))
+                        return null;
+
+            if (gridsAlready.All(gr =>
+            {
+                for (var j = 0; j < 25; j++)
+                    if (gr[j] != grid[j])
+                        return true;
+                return false;
+            }))
+                return grid;
+
+            return null;
+        }
+        var pixel = rnd.Next(0, 2) != 0;
+        grid[ix] = pixel;
+        var success = findGrid(grid, ix + 1, gridsAlready, rnd);
+        if (success != null)
+            return success;
+        grid[ix] = !pixel;
+        return findGrid(grid, ix + 1, gridsAlready, rnd);
+    }
+
 
     void Start()
     {
@@ -116,9 +184,9 @@ public class MandMs : MonoBehaviour
     }
 
     // Twitch Plays
-    #pragma warning disable 414
+#pragma warning disable 414
     private readonly string TwitchHelpMessage = "!{0} ";
-    #pragma warning restore 414
+#pragma warning restore 414
 
     IEnumerator ProcessTwitchCommand(string input)
     {
